e55669690c084a5e1da618bda4858663a3010ed1
Shahbaz Ahmad <139037350+shahbazahmad-786@users.noreply.github.com>
2024-12-30 13:38:10 +0500

    routes changed

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,8 @@ use App\Http\Controllers\UserProfileController;
 Route::prefix('v1')->group(function () {
     // Authentication routes
     Route::controller(UserController::class)->group(function () {
-        Route::post('/register', 'register')->name('register');
-        Route::post('/login', 'login')->name('login');
-        Route::post('/logout', 'logout')->name('logout');
+        Route::post('/register', 'register');
+        Route::post('/login', 'login');
     });
 
     // Protected routes
@@ -19,16 +18,17 @@ Route::prefix('v1')->group(function () {
         Route::controller(UserController::class)->group(function () {
             Route::get('/user', function (Request $request) {
                 return $request->user();
-            })->name('user');
+            });
 
-            Route::put('/change-password', 'changePassword')->name('changePassword');
-            Route::delete('/delete-account', 'deleteAccount')->name('deleteAccount');
+            Route::post('/logout', 'logout');
+            Route::put('/change-password', 'changePassword');
+            Route::delete('/delete-account', 'deleteAccount');
         });
 
         // Profile-related routes
         Route::controller(UserProfileController::class)->group(function () {
-            Route::put('/profile/update', 'updateInformation')->name('updateProfile');
-            Route::put('/social-media/update', 'updateSocialMedia')->name('updateSocialMedia');
+            Route::put('/profile/update', 'updateInformation');
+            Route::put('/social-media/update', 'updateSocialMedia');
         });
     });
 });
