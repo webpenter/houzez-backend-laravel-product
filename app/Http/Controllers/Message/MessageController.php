4518dee6e62f2e424f9668c2e0636255d7dec689
Naraish Kumar <rajnaresh329@gmail.com>
2025-02-05 11:56:48 +0500

    update

--- app/Http/Controllers/Message/MessageController.php
+++ app/Http/Controllers/Message/MessageController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Message;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Message\MessageResource;
-use App\Http\Resources\Message\MessageCollection;
 use App\Models\Message;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
